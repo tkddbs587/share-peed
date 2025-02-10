ef2ce59527209e0c514af5aa1b2bf0a671c96a60
hoon <dudrn587@gmail.com>
2025-02-10 23:04:48 +0900

    feat: 회원가입 비즈니스 로직 구현

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,20 +1,33 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
 type InputProps = {
+  type: string;
   name: string;
   label: string;
-  type: string;
+  errorMessage?: string | boolean;
+  register?: UseFormRegisterReturn;
   placeholder: string;
 };
 
-const Input = ({ name, label, type, placeholder }: InputProps) => {
+const Input = ({
+  type,
+  name,
+  label,
+  errorMessage,
+  register,
+  placeholder,
+}: InputProps) => {
   return (
     <div className="flex flex-col gap-4">
       <label>{label}</label>
       <input
-        name={name}
-        className="h-42 w-full rounded-4 border border-gray-30 p-8 pl-12 text-16-400"
         type={type}
+        name={name}
+        {...register}
         placeholder={placeholder}
+        className="h-42 w-full rounded-4 border border-gray-30 p-8 pl-12 text-16-400"
       />
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
     </div>
   );
 };
