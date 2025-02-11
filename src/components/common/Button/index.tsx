ffbe061302f2f52e3c692a3baff91fb7120e768e
hoon <dudrn587@gmail.com>
2025-02-12 00:34:44 +0900

    refactor: 버튼 props 수정

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,11 +1,17 @@
 type ButtonProps = {
   text: string;
+  buttonStyle?: string;
+  onClick?: () => void;
+  isLoading?: boolean;
 };
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, buttonStyle, onClick, isLoading }: ButtonProps) => {
   return (
-    <button className="rounded-4 bg-blue-500 p-13 text-14-400 text-white">
-      {text}
+    <button
+      onClick={onClick}
+      className={`${buttonStyle} rounded-4 p-13 text-14-400 text-white`}
+    >
+      {isLoading ? <span>Loding...</span> : text}
     </button>
   );
 };
