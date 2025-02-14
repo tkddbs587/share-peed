4d12ed72e8e8c5a1861ad85d09623d15bc451c61
hoon <dudrn587@gmail.com>
2025-02-14 21:12:30 +0900

    feat: 유저 정보 쿼리하여 유저 아바타 렌더링까지 구현

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -2,14 +2,20 @@ import { db } from '@/firebase';
 import { Unsubscribe } from 'firebase/auth';
 import {
   collection,
+  getDoc,
   limit,
   onSnapshot,
   orderBy,
   query,
+  doc as firestoreDoc,
 } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import UserPost from './UserPost';
 
+interface UserData {
+  avatarUrl?: string;
+  displayName: string;
+}
 export interface Post {
   id: string;
   userId: string;
@@ -17,6 +23,7 @@ export interface Post {
   photo?: string;
   content: string;
   createdAt: number;
+  avatarUrl?: string;
 }
 
 const TimeLine = () => {
@@ -30,18 +37,28 @@ const TimeLine = () => {
         orderBy('createdAt', 'desc'),
         limit(10),
       );
-      unsubscribe = await onSnapshot(postQuery, (snapshot) => {
-        const posts = snapshot.docs.map((doc) => {
-          const { content, createdAt, photo, userId, username } = doc.data();
+
+      unsubscribe = await onSnapshot(postQuery, async (snapshot) => {
+        const postsPromises = snapshot.docs.map(async (document) => {
+          const { content, createdAt, photo, userId, username } =
+            document.data();
+
+          // 유저의 프로필 정보 가져오기
+          const userDocRef = firestoreDoc(db, 'users', userId);
+          const userDocSnap = await getDoc(userDocRef);
+          const userData = userDocSnap.data() as UserData | undefined;
+
           return {
-            id: doc.id,
+            id: document.id,
             userId,
             username,
             photo,
             content,
             createdAt,
+            avatarUrl: userData?.avatarUrl,
           };
         });
+        const posts = await Promise.all(postsPromises);
         setPostsData(posts);
       });
     };
@@ -56,7 +73,7 @@ const TimeLine = () => {
   }, []);
 
   return (
-    <div>
+    <div className="mt-16">
       {postsData.map((post) => (
         <UserPost key={post.id} {...post} />
       ))}
--- src/components/UserPost.tsx
+++ src/components/UserPost.tsx
@@ -1,12 +1,29 @@
 import { Post } from './TimeLine';
 
-const UserPost = ({ id, userId, username, photo, content }: Post) => {
+const UserPost = ({
+  id,
+  userId,
+  username,
+  photo,
+  content,
+  avatarUrl,
+}: Post) => {
   return (
-    <div className="rounded-8 border border-gray-30 p-24">
-      <img className="rounded-full" />
-      <span>{username}</span>
-      <p>{content}</p>
-      {photo ? <img src={photo} className="h-192 w-260" /> : null}
+    <div className="mt-16 flex flex-col gap-16 rounded-8 border border-gray-30 p-24">
+      <div className="flex gap-12">
+        <img src={avatarUrl} className="h-40 w-40 rounded-full" />
+        <span className="text-16-400">{username}</span>
+      </div>
+      <p className="text-16-400">{content}</p>
+      {photo ? <img src={photo} className="h-192 w-full rounded-8" /> : null}
+      <div className="flex gap-16">
+        {/** to do 좋아요 개수 DB 저장해서 불러오기 */}
+        <img src="/src/assets/icons/heart.svg" />
+        {/** to do 댓글 개수 DB 저장해서 불러오기, 클릭 시 댓글 목록 모달 띄우기*/}
+        <img src="/src/assets/icons/comment.svg" />
+        {/** to do 공유 기능 알아보기 */}
+        <img src="/src/assets/icons/share.svg" />
+      </div>
     </div>
   );
 };
